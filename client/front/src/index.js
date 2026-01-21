d9417b6cf7b2b23e013ec974fe5a7319ad599c92
SpookyRon <gximena97@gmail.com>
2026-01-21 01:59:43 +0100

    changes html

--- client/front/src/index.js
+++ client/front/src/index.js
@@ -1,3 +1,3 @@
-import './components/font-loader.js'
-import './components/header.js'
-import './components/footer.js'
+import './components/font-loader-component.js'
+import './components/header-component.js'
+import './components/footer-component.js'
